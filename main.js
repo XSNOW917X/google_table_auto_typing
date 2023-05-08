dc85852a702e48f302465268b3732e4b4e6d2bc0
clevens <a0903325903z@gmail.com>
2023-05-09 00:21:01 +0800

    completeV1

---
+++ main.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         Google_auro_table
+// @namespace    https://your-namespace-here
+// @version      1.0
+// @description  NMSL
+// @match        https://docs.google.com/forms/d/e/1FAIpQLSchABfHBEv18-RSYHhCHspGb6RFyRze9wDdzBm4nQgea0U2XA/viewform*
+// @grant        none
+// ==/UserScript==
+
+
+//document.querySelector("#i8").click();
+setTimeout(() => {
+    const selectors = [
+        '#i5', '#i8', '#i11', '#i15',
+        '#i21', '#i24', '#i27'
+    ];
+
+    const randomIndex1 = Math.floor(Math.random() * 4);
+    document.querySelector(selectors[randomIndex1]).click();
+
+    const randomIndex2 = Math.floor(Math.random() * 3) + 4;
+    document.querySelector(selectors[randomIndex2]).click();
+}, 2500);
+
+setTimeout(() => {
+    document.forms[0].submit();
+},3000);
+
---
+++ reload.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Google_auro_table_reload
+// @namespace    https://your-namespace-here
+// @version      1.0
+// @description  NMSL
+// @match        https://docs.google.com/forms/u/0/d/e/1FAIpQLSchABfHBEv18-RSYHhCHspGb6RFyRze9wDdzBm4nQgea0U2XA/formResponse
+// @grant        none
+// ==/UserScript==
+setTimeout(() => {
+    window.location.reload();
+    }, 2000);
+    
+    
